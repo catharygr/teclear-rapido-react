db37a9dbc4895aa320fb3f70a40b3ac3ac368d99
catharygr <103951447+catharygr@users.noreply.github.com>
2023-03-06 13:14:38 +0000

    Maquetación y estilos terminado

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 function App() {
-  return <p>Hola</p>;
+  return (
+    <div className="container">
+      <h1>Qué tan rápido puedes teclear?</h1>
+      <textarea />
+      <h3>Tiempo restante: 0</h3>
+      <button>Comenzar</button>
+      <h2>Palabras contada: 0</h2>
+    </div>
+  );
 }
 
 export default App;
