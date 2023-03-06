18f1f05d7beb82f8a21028acc6c2478b139f8a35
catharygr <103951447+catharygr@users.noreply.github.com>
2023-03-06 12:35:45 +0000

    Commit inicial setup Vite, Eslint, Prettier

---
+++ README.md
@@ -0,0 +1,1 @@
+# Proyecto App de medir palabras escrita en un tiempo determinado hechop con React 18
---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function App() {
+  return <p>Hola</p>;
+}
+
+export default App;
